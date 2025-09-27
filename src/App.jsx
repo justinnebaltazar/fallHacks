d894deacc38f0c700edaa66e43aa4fcd05752d15
Justinne Baltazar <jfb3@sfu.ca>
2025-09-27 09:59:59 -0700

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      <div>hello world</div>
-    </>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          {/* home page */}
+          <Route path="/" element={
+            <>
+              <Navbar/>
+              <Home/>
+            </>
+          }>
+          </Route>
+          
+          {/* login */}
+          <Route path="/login" element={
+            <>
+              <Login/>
+            </>
+          }>
+          </Route>
+
+          <Route path="/register" element={
+            <>
+              <Register/>
+            </>
+          }>
+          </Route>
+      
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
