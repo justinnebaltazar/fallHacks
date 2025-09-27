243156211b4b37eac719c9ac6190b37ffd4b65d6
Rio <riomaruyama@hotmail.com>
2025-09-27 16:20:26 -0700

    removed drop down png

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import supabase from "../helper/supabaseClient";
 import { useDropzone } from 'react-dropzone'
-import dropdown from '../assets/dropdown.png'
 
 export const Upload = ({trigger, onClose, onDone}) => {
     const [files, setFiles] = useState([])
