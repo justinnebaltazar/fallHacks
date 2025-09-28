8f478c4b4278ebc3da193c242d48d74e416b7144
Rio <riomaruyama@hotmail.com>
2025-09-27 18:14:20 -0700

    tried to complete backend

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -8,50 +8,65 @@ export const Upload = ({trigger, onClose}) => {
     const [dresscode, setDresscode] = useState("Dress Code")
     const [pants, setPants] = useState("Pants Length")
     const [sleeves, setSleeves] = useState("Sleeve Length")
-    const [user, setUser] = useState([])
-    const [outfit, setOutfit] = useState([])
 
-    useEffect(() => {
-        const fetchOutfit = async () => {
-            if (!selectedOutfit) return;
-            const { data, error } = await supabase
-            .from("outfit")
-            .select("*")
-        }
-    })
 
     const {getRootProps, getInputProps} = 
     useDropzone({
         accept: {
-            'image/*':[]
+            'image/*':[],
         },
+        multiple: false,
+
         onDrop: acceptedFiles => {
-            setFiles(acceptedFiles.map(file =>
-                Object.assign(file, {
-                    preview: URL.createObjectURL(file)
-                })
-            ))
+            const file = acceptedFiles[0]
+            if (file) {
+                file.preview = URL.createObjectURL(file);
+                setFiles([file])
+            }
         }
     });
-    const thumbs = files.map(file => (
-        <div className="" key={file.name}>
-            <div>
-                <img
-                    src={file.preview}
-                    onLoad={() => {
-                        URL.revokeObjectURL(file.preview)
-                    }}/>
-            </div>
-        </div>
-    ));
 
     useEffect(() => {
         return () => files.forEach(file => 
             URL.revokeObjectURL(file.preview))
     }, [files])
 
-    const handleSubmit = () => {
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (!files[0]) {
+            alert("Please select a file!")
+            return;
+        }
+
+        const file = files[0]
+        const fileName = `${Date.now()}-${file.name}`;
         
+        try {
+            const { data, error } = await supabase
+                .storage
+                .from("clothes")
+                .upload(fileName, file);
+            
+            if (error) throw error
+
+
+            const { url, urlError } = await supabase
+                .storage
+                .from("clothes")
+                .getPublicUrl(fileName);
+
+            if (urlError) throw urlError
+
+            const publicUrl = url.publicUrl;
+
+            const { data1, error1 } = await supabase
+                .from("outfit")
+                .insert([{image_data: publicUrl, dress_code: dresscode, clothing_type: type, pants: pants, sleeve_length: sleeves}])
+        } catch (err) {
+        }
+        
+        onClose()
     }
 
     if (!trigger) return null
@@ -66,43 +81,46 @@ export const Upload = ({trigger, onClose}) => {
                         <div className="bg-[#5885AF]">
                             <input {...getInputProps()}/>
                             <p className="text-white">Drop or select your files</p>
+                            {files[0] && (
+                                <img src={files[0].preview} className="w-32 h-32 object-cover rounded mb-2" />
+                            )}
                         </div>
                     </div>
 
                     {/* form */}
                     <div>
                         <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-                            
-                            <select value={dresscode} onChange={(e) => setDresscode(e.target.event)} className="justify-between items-center unset bg-[#D9D9D9] text-[#274472] p-3 rounded-lg">
-                                <option value="" selected>select</option>
+
+                            <select value={dresscode} onChange={(e) => setDresscode(e.target.value)} required className="justify-between items-center unset bg-[#D9D9D9] text-[#274472] p-3 rounded-lg">
+                                <option value="">select</option>
                                 <option value="casual">casual</option>
                                 <option value="semi-casual">semi-casual</option>
                                 <option value="formal">formal</option>
                             </select>
 
-                            <select value={type} onChange={(e) => setType(e.target.event)} className="justify-between items-center unset bg-[#D9D9D9] text-[#274472] p-3 rounded-lg">
-                                <option value="" selected>select</option>
+                            <select value={type} onChange={(e) => setType(e.target.value)} required className="justify-between items-center unset bg-[#D9D9D9] text-[#274472] p-3 rounded-lg">
+                                <option value="">select</option>
                                 <option value="top">top</option>
                                 <option value="pants">pants</option>
                                 <option value="shoes">shoes</option>
                             </select>
 
-                            <select value={pants} onChange={(e) => setPants(e.target.event)} className="justify-between items-center unset bg-[#D9D9D9] text-[#274472] p-3 rounded-lg">
-                                <option value="" selected>select</option>
+                            <select value={pants} onChange={(e) => setPants(e.target.value)} className="justify-between items-center unset bg-[#D9D9D9] text-[#274472] p-3 rounded-lg">
+                                <option value="">select</option>
                                 <option value="ankle">ankle length</option>
                                 <option value="knee-length">knee length</option>
                                 <option value="shorts">shorts</option>
                             </select>
 
-                            <select value={sleeves} onChange={(e) => setSleeves(e.target.event)} className="justify-between items-center unset bg-[#D9D9D9] text-[#274472] p-3 rounded-lg">
-                                <option value="" selected>select</option>
+                            <select value={sleeves} onChange={(e) => setSleeves(e.target.value)} className="justify-between items-center unset bg-[#D9D9D9] text-[#274472] p-3 rounded-lg">
+                                <option value="">select</option>
                                 <option value="tank-top">tank-top</option>
                                 <option value="t-shirt">t-shirt</option>
                                 <option value="elbow-length">elbow-length</option>
                                 <option value="long-sleeve">long sleeve</option>
                             </select>
 
-                            <button onClick={() => handleSubmit} className="cursor-pointer justify-between items-center unset bg-[#274472] text-[#D9D9D9] p-3 rounded-lg">Submit</button>
+                            <button type = "submit" className="cursor-pointer justify-between items-center unset bg-[#274472] text-[#D9D9D9] p-3 rounded-lg">Submit</button>
                             <button onClick={onClose} type="button" className="cursor-pointer justify-between items-center unset bg-[#41729F] text-[#D9D9D9] p-3 rounded-lg">Cancel</button>
                         </form>
                     </div>
